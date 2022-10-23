978855f2e62b2091c36cdb7c717f48c1ed3c83c1
AthreyaV <40580892+AthreyaV@users.noreply.github.com>
2022-10-23 12:22:06 -0500

    Update main.ts
    changed txt to js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { Plugin } from 'obsidian';
 
-export default class TxtAsMdPlugin extends Plugin {
+export default class JsAsMdPlugin extends Plugin {
 
   async onload() {
     super.onload();
 
     // register the view and extensions
-    this.registerExtensions(["txt"], "markdown");
+    this.registerExtensions(["js"], "markdown");
   }
 }
